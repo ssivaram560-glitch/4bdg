340d6047c3ea2a9b2c607861745f8ac59191fbab
ssivaram560-glitch <ssivaram560@gmail.com>
2026-09-08 18:46:47 +0530

    Hardcode BOT_TOKEN and OWNER_PASS values
    Replaced environment variables with hardcoded values for BOT_TOKEN and OWNER_PASS.

--- bot.js
+++ bot.js
@@ -639,9 +639,9 @@ async function captchaLogin(userId, chatId, phone, password, bot, logBoth) {
 //  CONFIG
 // ============================================================
 // Keep secrets outside the source code.
-const BOT_TOKEN    = process.env.BOT_TOKEN || "";
+const BOT_TOKEN    = process.env.BOT_TOKEN || "8670635800:AAEeDoWmav3IL5Pj19shmaSfTHNuLjaT9Lw";
 const OWNER_ID     = 8869874751;
-const OWNER_PASS   = process.env.OWNER_PASS || "";
+const OWNER_PASS   = process.env.OWNER_PASS || "2004";
 const ADMIN_HANDLE = "@Sivakutty1";
 const REG_LINK     = process.env.REG_LINK || "https://13llottery.com";
 const WIN_STICKER  = "CAACAgUAAxkBAAFHUGNp4JX1-ohP4uBEWpfNptaz-HmwVgAC4hgAAhboKVbObuGuTcMs2zsE";
